741ee11555c406d5435579c4c388478e0637efd9
Tasha <tmb15@humboldt.edu>
2024-04-29 02:30:29 -0700

    :)
    im so close to completing the basic calculator

--- Simple_calc.php
+++ Simple_calc.php
@@ -1,30 +1,39 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-    $Operand1=$_POST['Operand1'];
-    $Operand2=$_POST['Operand2'];
-    $Operator=$_POST['Calculate'];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basic Calculator</title>
+</head>
+<body>
+    <h2>Result:</h2>
+    <?php
+    if (isset($_POST['submit'])) {
+        $num1 = $_POST['num1'];
+        $operator = $_POST['operator'];
 
-    /*Validation begins from here.*/
-    if($Operand1 == '' || $Operand2 == ''){
-        $Error = "The input values are required.";
+        switch ($operator) {
+            case '+':
+                $result = $num1 + $num2;
+                break;
+            case '-':
+                $result = $num1 - $num2;
+                break;
+            case '*':
+                $result = $num1 * $num2;
+                break;
+            case '/':
+                if ($num2 == 0) {
+                    $result = "Cannot divide by zero";
+                } else {
+                    $result = $num1 / $num2;
+                }
+                break;
+            default:
+                $result = "Invalid operator";
+        }
+        echo "Result: " . $result;
     }
-    elseif (filter_var($Operand1, FILTER_VALIDATE_FLOAT) === false || filter_var($Operand2, FILTER_VALIDATE_FLOAT) === false) {
-        $Error = "The input value must be a number only.";
-    }
-    elseif($Operator=="/" && ($Operand1 == 0 || $Operand2 == 0)){
-        $Error = "Cannot divide by zero.";
-    }
-    else{
-        /*Calculation begins from here.*/
-        if($Operator=="+")
-            $Result=$Operand1+$Operand2;
-        else if($Operator=="-")
-            $Result=$Operand1-$Operand2;
-        else if($Operator=="x")
-            $Result=$Operand1*$Operand2;
-        else if($Operator=="/")
-            $Result=$Operand1/$Operand2;
-    }
-} 
-?>
+    ?>
+</body>
+</html>
