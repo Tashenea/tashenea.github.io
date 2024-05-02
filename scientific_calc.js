06c8f9c2e59fc375db005eed37adfd0898ddea0a
AntonioGallegos01 <antonio.gallegos063@myci.csuci.edu>
2024-05-02 14:17:10 -0700

    added 3 new files testing out some new format

---
+++ scientific_calc.js
@@ -0,0 +1,6 @@
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .output');
+function backspc
+{
+    display.vualue = display.value.toString().slice(0,-1);
+}
