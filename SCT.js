3db70422d962006a18dfec7cba4f727920ca177f
AntonioGallegos01 <antonio.gallegos063@myci.csuci.edu>
2024-05-08 23:25:30 -0700

    Got the new format to connect with the java file. Currently just adding the rest of the functions.

--- SCT.js
+++ SCT.js
@@ -1,11 +1,30 @@
-var screen = document.querySelectorAll('#screen');
-var calcBtn = document.querySelectorAll('.btn')
+var screen=document.querySelector('#calcScrn');
+var btn=document.querySelectorAll('.calcBtn');
 
-for(item of calcBtn)
+for(item of btn)
 {
 	item.addEventListener('click',(e)=>{
-		btntext = e.target.innerText;
+		btntext=e.target.innerText;
+		if(btntext == 'x')
+		{
+			btntext='*';
+		}
+		if(btntext == '÷')
+		{
+			btntext='/';
+		}
+
+
 		screen.value+=btntext;
 
 	});
+}
+function clrAll()
+{
+	screen.value = '';
+}
+
+function equalOperator()
+{
+	screen.value = eval(screen.value);
 }
